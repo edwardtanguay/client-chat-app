e6776b2b80abe1c34f7d3aa22451a81e5ba6da97
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-15 11:24:41 +0100

    redeploy

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -4,96 +4,97 @@ import { useState } from "react";
 //import { useNavigate } from "react-router-dom";
 
 function Signup() {
-  //const navigate = useNavigate();
-  const [message, setMessage] = useState("");
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
+	//const navigate = useNavigate();
+	const [message, setMessage] = useState("");
+	const [values, setValues] = useState({
+		username: "",
+		email: "",
+		password: "",
+		password2: "",
+	});
 
-  const [errors, setErrors] = useState({});
+	const [errors, setErrors] = useState({});
 
-  const clearForm = () => {
-    setValues({ username: "", email: "", password: "", password2: "" });
-    setErrors({});
-  };
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setErrors(validate(values));
-    if (Object.keys(errors).length === 0) {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/signup`, {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          user: {
-            name: values.username,
-            email: values.email,
-            password: values.password,
-          },
-        }),
-      });
-      if (response.ok) {
-        setMessage( `Hi ${values.username}, please confirm your email`
-        );
-        clearForm();
-      }
-    }
-  };
-  return (
-    <div className="signup">
+	const clearForm = () => {
+		setValues({ username: "", email: "", password: "", password2: "" });
+		setErrors({});
+	};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setValues({
+			...values,
+			[name]: value,
+		});
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setErrors(validate(values));
+		if (Object.keys(errors).length === 0) {
+			const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/signup`, {
+				method: "POST",
+				credentials: "include",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					user: {
+						name: values.username,
+						email: values.email,
+						password: values.password,
+					},
+				}),
+			});
+			if (response.ok) {
+				setMessage(`Hi ${values.username}, please confirm your email`
+				);
+				clearForm();
+			}
+		}
+	};
+	return (
+		<div className="signup">
 			<div>test1: [{process.env.REACT_APP_BACKEND_URL}] </div>
 			<div>test2: [{process.env.REACT_APP_TEST}] </div>
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          name="username"
-          type="text"
-          placeholder="username"
-          value={values.username}
-          onChange={handleChange}
-        />
-        {errors.username && <p>{errors.username}</p>}
+			<div>test for deploy</div>
+			<form action="" onSubmit={handleSubmit}>
+				<input
+					name="username"
+					type="text"
+					placeholder="username"
+					value={values.username}
+					onChange={handleChange}
+				/>
+				{errors.username && <p>{errors.username}</p>}
 
-        <input
-          name="email"
-          type="email"
-          placeholder="email"
-          value={values.email}
-          onChange={handleChange}
-        />
-        {errors.email && <p>{errors.email}</p>}
+				<input
+					name="email"
+					type="email"
+					placeholder="email"
+					value={values.email}
+					onChange={handleChange}
+				/>
+				{errors.email && <p>{errors.email}</p>}
 
-        <input
-          name="password"
-          type="password"
-          placeholder="password"
-          value={values.password}
-          onChange={handleChange}
-        />
-        {errors.password && <p>{errors.password}</p>}
+				<input
+					name="password"
+					type="password"
+					placeholder="password"
+					value={values.password}
+					onChange={handleChange}
+				/>
+				{errors.password && <p>{errors.password}</p>}
 
-        <input
-          name="password2"
-          type="password"
-          placeholder="confirm password"
-          value={values.password2}
-          onChange={handleChange}
-        />
-        {errors.password2 && <p>{errors.password2}</p>}
+				<input
+					name="password2"
+					type="password"
+					placeholder="confirm password"
+					value={values.password2}
+					onChange={handleChange}
+				/>
+				{errors.password2 && <p>{errors.password2}</p>}
 
-        <button type="submit">Create My Account</button>
-      </form>
-      <h3>{message}</h3>
-    </div>
-  );
+				<button type="submit">Create My Account</button>
+			</form>
+			<h3>{message}</h3>
+		</div>
+	);
 }
 export default Signup;
